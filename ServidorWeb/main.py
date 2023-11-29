5ff08282052b733af44de90888bed56d1e0e6183
Vinicius <viniaoliveira22@gmail.com>
2023-11-29 10:54:52 -0300

    first commit api

---
+++ ServidorWeb/main.py
@@ -0,0 +1,73 @@
+# Código para ser desenvolvido na feature
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class user(BaseModel):
+    id: int
+    nome: str
+    turno_livre: str
+    email: str
+    senha: str
+
+
+app = FastAPI()
+alunos = [
+    {
+        "id": 1,
+        "nome": "Vinicius",
+        "email": "vinicius@gmail.com",
+        "senha": "123456",
+        "turno_livre": "manhã"
+    },
+    {
+        "id": 2,
+        "nome": "Alessandro",
+        "email": "alessandro@gmail.com",
+        "senha": "789123",
+        "turno_livre": "noite"
+    }
+]
+
+
+@app.get("/user")
+def pesquisa():
+    return alunos
+
+
+@app.get("/user/{id}")
+def pesquisa_id(id: int):
+    for aluno in alunos:
+        if aluno['id'] == id:
+            return aluno
+    return "not found"
+
+
+@app.post("/user")
+def inserir(info: user):
+    alunos.append({
+        "id": info.id,
+        "nome": info.nome,
+        "email": info.email,
+        "senha": info.senha,
+        "turno_livre": info.turno_livre
+    })
+    return alunos
+
+
+@app.put("/user/id")
+def atualizar(info: user, id: int):
+    contador = 0
+    for aluno in alunos:
+        if aluno["id"] == id:
+            alunos[contador] = {
+                "id": info.id,
+                "nome": info.nome,
+                "email": info.email,
+                "senha": info.senha,
+                "turno_livre": info.turno_livre
+            }
+        return "atualizado"
+        contador += 1
+    return "produto inexistente"
