e29a590d72e762a491d4d34f4f971da01eaff3b7
Victor <eduardovictor822@gmail.com>
2023-12-04 22:20:28 -0300

    Tela Inicial 30%

--- AppMobile/chronusgram/scr/pages/tela_principal/Forms/index.js
+++ AppMobile/chronusgram/scr/pages/tela_principal/Forms/index.js
@@ -10,6 +10,32 @@ export default function Form() {
             <TouchableOpacity>
                 <Text> tela principal é aqui </Text>
             </TouchableOpacity>
+
+        <View style = {styles.Caixabaixo}>
+            <Text style = {styles.Titulo}>Perfomance</Text>
+        
+        <View style = {styles.CaixaDentroEsquerda}>
+
+        </View>
+            <Text style = {styles.Titulo2}>
+                Ofensiva
+            </Text>
+            <Text style = {styles.Titulo3}>15</Text>
+            <Text style = {styles.Titulo2}> Fogo 12%</Text>
+        </View>
+
+        <View style = {styles.CaixaDentroDireita}>
+            <Text style = {styles.Titulo4}>
+                    Constância
+            </Text>
+            <Text style = {styles.Titulo5}>70</Text>
+            <Text style = {styles.Titulo6}>24%</Text>
+        </View>
+
+
+
+
+
         </View>
     )
 }
--- AppMobile/chronusgram/scr/pages/tela_principal/Forms/style.js
+++ AppMobile/chronusgram/scr/pages/tela_principal/Forms/style.js
@@ -13,6 +13,72 @@ const styles = StyleSheet.create({
     Logo:{
         width: 200,
         height: 100,
+    },
+    Caixabaixo: {
+        width: 350,
+        height: 220,
+        top: 450,
+        borderRadius:20,
+        backgroundColor: '#73628A'
+    },
+    Titulo: {
+        width:200,
+        top: 24,
+        left: 30,
+        fontStyle:'italic',
+        color: '#FFFBFF'
+    },
+    Titulo2:{
+        width:200,
+        bottom: 35,
+        left: 30,
+        fontStyle:'italic',
+        color: '#1F2234'
+    },
+    Titulo3:{
+        width:120,
+        bottom: 35,
+        fontSize: 45,
+        left: 30,
+        color: '#1F2234'
+    },
+    Titulo4: {
+        width: 87,
+        height: 30,
+        top: 8,
+        left: 15,
+        color: '#E6E7ED'
+    },
+    Titulo5:{
+        width: 118,
+        height: 55,
+        left: 40,
+        fontSize: 45,
+        color: '#25D26A'
+    },
+    Titulo6: {
+        width:200,
+        top: 5,
+        left: 30,
+        fontStyle:'italic',
+        color: '#1F2234'
+    },
+    CaixaDentroEsquerda: {
+        width: 159,
+        height: 117,
+        top: 68,
+        left: 14,
+        borderRadius: 15,
+        backgroundColor:'#FFFFFF'
+
+    },
+    CaixaDentroDireita: {
+        width: 159,
+        height: 117,
+        top: 316,
+        left: 90,
+        borderRadius: 15,
+        backgroundColor:'#603F74'
     }
   });
 
