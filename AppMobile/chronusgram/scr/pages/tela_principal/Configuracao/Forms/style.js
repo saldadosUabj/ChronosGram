bcef351ca25949d5095685839362cf23b333f386
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2024-02-11 21:40:56 -0300

    Tela de Configuração Completa

--- AppMobile/chronusgram/scr/pages/tela_principal/Configuracao/Forms/style.js
+++ AppMobile/chronusgram/scr/pages/tela_principal/Configuracao/Forms/style.js
@@ -3,21 +3,56 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
     },
     text:{
         alignItems: 'flex-start',
-        marginLeft: 20,
-        fontSize: 16,
+        marginLeft: 48,
+        fontSize: 25,
         marginTop: 40,
         marginBottom: 20,
     },
     retangulo: {
+        alignSelf: 'center',
         width: 300,        // Largura do retângulo
         height: 180,       // Altura do retângulo
         backgroundColor: '#73628A',  // Cor de fundo do retângulo
         borderRadius: 15,    // Cor da borda (opcional)
       },
+    inputSelectdBox: {
+        width: '80%',
+        height: 'auto',
+        alignSelf: 'center',
+        backgroundColor: '#73628A',
+        marginTop: 15,
+        marginHorizontal: 10,
+        padding: 20,
+      },
+    inputBox: {
+        width: '85%',
+        height: 'auto',
+        alignSelf: 'center',
+        borderRadius: 15,
+        backgroundColor: '#73628A',
+        marginTop: 18,
+        marginHorizontal: 10,
+        padding: 15,
+    },
+    buttonEntrar:{
+        width:'70%' , 
+        backgroundColor: '#73628A',
+        alignSelf: 'center',
+        alignItems: 'center',
+        marginTop: 200,
+        borderRadius: 8,
+        padding: 5,
+    },
+    buttonText:{
+        color:'#fff',
+        fontFamily:'Nunito-Sans',
+        fontWeight:'bold',
+        fontSize:20,
+        padding:10,
+    },
 });
 
 export default styles
