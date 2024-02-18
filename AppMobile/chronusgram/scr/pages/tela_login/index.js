003dc40fcfe4f7faf3e7581d607f8f9fd3d92b87
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2024-02-18 11:27:06 -0300

    Resolvendo bug do react-gesture-handler

--- AppMobile/chronusgram/scr/pages/tela_login/index.js
+++ AppMobile/chronusgram/scr/pages/tela_login/index.js
@@ -2,6 +2,7 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import Logo from './logo/index'
 import Form from './Form/index'
 import { useFonts } from 'expo-font'
+import { GestureHandlerRootView} from 'react-native-gesture-handler'
 
 export default function App() {
 
