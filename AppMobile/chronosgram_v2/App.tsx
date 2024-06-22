9b03a82d63efc6237c99677f8b79935a62ca5ad2
Wandson | Notebook <wandson7emanuel@hotmail.com>
2024-06-22 01:34:41 -0300

    teste

--- AppMobile/chronosgram_v2/App.tsx
+++ AppMobile/chronosgram_v2/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>O Chronosgram V2 Começou por aqui, que Deus nos abençoe</Text>
+      <Text style={styles.text}>O Chronosgram V2 Começou por aqui, que Deus nos abençoe</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#16041B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#fff',
+  }
 });
