22fde49212ef72c070db5e1542df5b460ca849ee
Vinicius <viniaoliveira22@gmail.com>
2024-07-11 22:32:59 -0300

    feat: implemented tela rotinas

--- AppMobile/chronosgram/src/pages/home/rotinas/rotinas.tsx
+++ AppMobile/chronosgram/src/pages/home/rotinas/rotinas.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import styles from './style';
@@ -8,11 +8,123 @@ export default function Rotinas() {
   const navigation = useNavigation();
 
   return (
-    
-    <View style={styles.container}>
-     
-      <Text> Rotinas </Text>
-      
-    </View>
+    <KeyboardAvoidingView style={styles.body}>
+      <View style={styles.FirstView}>
+        <Text style={styles.Traco}> </Text>
+        <View style={styles.myRoutines}>
+          <Text style={styles.TitleMyRoutines}> Minhas Rotinas </Text>
+          <ScrollView horizontal={true}>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/estagiarios.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina} >Rotina para estagiários</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>500 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Ver</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/estagiarios.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina} >Rotina para estagiários</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>500 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Ver</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.Traco}> </Text>
+        <View style={styles.ChooseRoutine}>
+          <Text style={styles.TituloRotina}> Rotinas Sugeridas </Text>
+          <ScrollView>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/livros.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina} >Melhor rotina do mundo</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>150 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Entrar</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/engenharia.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina} >Rotina para Engenheiros</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>221 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Entrar</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/estagiarios.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina} >Rotina para estagiários</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>500 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Entrou</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.allOfOneRoutine}>
+              <Image style={styles.imgMyRoutine} source={require('../../../../assets/images/rotinas/hidrica.jpg')} />
+              <View style={styles.PartOfOneRoutineColumn}>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.TituloRotina}>Rotina para Hidrica</Text>
+                </View>
+                <View style={styles.PartOfOneRoutineRow}>
+                  <Text style={styles.By}>by:</Text>
+                  <Image style={styles.imgPerfil} source={require('../../../../assets/images/rotinas/perfil.png')} />
+                  <Image style={styles.imgElipse} source={require('../../../../assets/images/rotinas/elipse.png')} />
+                  <Text style={{ color: "white" }}>14 Junções</Text>
+                </View>
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.Button}>Entrar</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    </KeyboardAvoidingView >
   );
 }
--- AppMobile/chronosgram/src/pages/home/rotinas/style.tsx
+++ AppMobile/chronosgram/src/pages/home/rotinas/style.tsx
@@ -1,12 +1,100 @@
 import { StyleSheet } from "react-native";
+import { white } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },     
+    body:{
+      flex: 1,
+      backgroundColor: '#16041B',
+      
+      
+    },
+    FirstView:{
+      marginTop:50
+    },
+    imgMyRoutine:{
+      width:30,
+      height:40,
+      borderBottomLeftRadius:10,
+      borderTopLeftRadius:10
+    },
+    imgChooseRoutine:{
+      width:51,
+      height:53
+    },
+    imgElipse:{
+      width:5,
+      height:5,
+      marginTop:8,
+      marginLeft:10
+    },
+    imgPerfil:{
+      width:10,
+      height:10,
+      marginTop:5
+    },
+    Traco:{
+      backgroundColor:"white",
+      justifyContent:"space-between",
+      height:2,
+      marginTop:10
+    },
+    TituloRotina:{
+      color: "white",
+      marginLeft:10,
+      marginRight:10
+    },
+    allOfOneRoutine:{
+     width:300,
+     marginTop:10,
+     backgroundColor:"#8663B4",
+     justifyContent:"space-between",
+     flexDirection:"row",
+     borderWidth:1,
+     borderColor:"black",
+     borderStyle:"solid",
+     borderRadius:10,
+     alignItems:"center",
+     marginLeft:5
+     
+    },
+    PartOfOneRoutineRow:{
+      flexDirection:"row",
+      
+    },
+    PartOfOneRoutineColumn:{
+      flexDirection:"column",
+    },
+    By:{
+      paddingLeft:5
+    },
+    myRoutines:{
+      justifyContent:"space-between",
+      marginLeft:10
+
+    },
+    TitleMyRoutines:{
+      color:"white",
+      textAlign:"center",
+      
+    },
+    ChooseRoutine:{
+      marginTop:10,
+      justifyContent:"space-between",
+      alignItems:"center"
+      
+
+    },
+    Button:{
+      backgroundColor:"#B8B8B8",
+      width:65,
+      height:40,
+      borderTopRightRadius:10,
+      borderBottomRightRadius:10,
+      textAlign:"center",
+      padding:10
+    }
+    
+    
   });
 
 export default styles
