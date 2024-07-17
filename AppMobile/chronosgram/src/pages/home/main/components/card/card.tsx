30b80b73fd2bcb7ecd305fbc0fcf004068668aa6
Wandson | Notebook <wandson7emanuel@hotmail.com>
2024-07-17 15:46:11 -0300

    card finalizado | principal 80%

--- AppMobile/chronosgram/src/pages/home/main/components/card/card.tsx
+++ AppMobile/chronosgram/src/pages/home/main/components/card/card.tsx
@@ -1,5 +1,5 @@
 import { ScrollView ,StyleSheet, Text, TouchableOpacity, View, Image, ImageBackground } from 'react-native';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import styles from './style';
@@ -9,13 +9,66 @@ import { AuthContext } from '../../../../../contexts/auth';
 export default function Card() {
 
   const { nome } = useContext<any>(AuthContext)
+  var [keystatus, setKeyStatus] = useState<any>('')
+  const tempo  = '30 min';
+  const titulo = 'Prova de Calculo 1';
+  const coins = '120 coins';
+  const status = 'Em dia';
+  const instrucao = 'Primeira parte Lista'
+
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
+
         <View style={styles.containerHorizontal}>
-          <ImageBackground style= {styles.backgroung} imageStyle={styles.backgroundImage}source={require('../../../../../../assets/images/header-card.jpg')}/>                 
+          <ImageBackground style= {styles.backgroung} imageStyle={styles.backgroundImage}source={require('../../../../../../assets/images/header-card.jpg')} resizeMode='cover'>
+
+            <View style={styles.containerHorizontal}>
+              
+              <Image style={styles.relogio} source={require('../../../../../../assets/images/relogio.png')}/>
+              <Text style={styles.textoSecundario}> {tempo} </Text>
+              <Text style={styles.textoPrimario}> {titulo} </Text>
+              <Image style={styles.moeda} source={require('../../../../../../assets/images/moeda.png')}/>
+              <Text style={styles.textoSecundario}> {coins} </Text>
+              
+            </View>
+
+          </ImageBackground>             
         </View>
+
+        <View>
+          <View style={styles.constainerStatus}>
+            <Text style={[styles.textoSecundario,{alignSelf: 'center'}]}> {status} </Text>
+          </View>
+        </View>
+
+        <View style={{justifyContent: 'flex-start', flexDirection: 'row', padding: 10}}>
+
+          <Image style={styles.trofeu} source={require('../../../../../../assets/images/trofeu.png')}/>
+          <Text style={[styles.textoPrimario,{margin: 2, marginVertical: 5}]}> {instrucao} </Text>
+
+        </View>
+
+        <View style={{flex:1 ,flexDirection: 'row', justifyContent: 'space-between'}}>
+
+        <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+
+          
+
+        </View>
+
+          <View style={{flexDirection: 'row', alignSelf: 'center', margin: 5}}>
+
+            <Image style={styles.miniaturas} source={require('../../../../../../assets/images/joinha.png')}/>
+            <Image style={styles.miniaturas} source={require('../../../../../../assets/images/negativo.png')}/>
+            <Image style={styles.miniaturas} source={require('../../../../../../assets/images/comentario.png')}/>
+
+          </View>
+
+
+        </View>
+
     </View>
   );
 }
--- AppMobile/chronosgram/src/pages/home/main/components/card/style.tsx
+++ AppMobile/chronosgram/src/pages/home/main/components/card/style.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 
+const fontePrimaria = 'Josefins-Sans-Bold'
 const fundo = "#16041B"
 const card = "#73628A"
 const secundaria = "#603F74"
@@ -21,12 +22,49 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 20
     },
     containerHorizontal: {
-        borderRadius: 20,
-        height: 60,
-        flexDirection: "row",
+      justifyContent: 'center',    
+      borderRadius: 20,
+      height: 60,
+      flexDirection: "row",
+    },
+    constainerStatus:{
+      backgroundColor: verde,
+      borderBottomRightRadius: 20,
+      width: 150,
+      justifyContent: 'flex-start',
     },
     fundo:{
         flex: 1,
+    },
+    relogio:{
+      width: 25,
+      height: 25,
+      alignSelf: 'center',
+    },
+    moeda:{
+      marginVertical: 18,
+      alignSelf: 'center',
+    },
+    trofeu:{
+      alignSelf: 'center',
+      marginLeft: 15,
+    },
+    miniaturas:{
+      margin:10,
+    },
+    textoPrimario:{
+      alignSelf: 'center',
+      fontFamily: fontePrimaria,
+      color: 'white',
+      fontSize: 18,
+      marginVertical: 15,
+      marginHorizontal: 8,
+    },
+    textoSecundario:{
+      fontFamily: fontePrimaria,
+      color: 'white',
+      alignSelf: 'center',
+      fontSize: 12,
     }
   });
 
