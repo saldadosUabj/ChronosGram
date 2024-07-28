b47f20706b11ff50c7bbfb42138597e8b20324cb
Matheus Patriota <109482342+Matth-P@users.noreply.github.com>
2024-07-28 00:02:27 -0300

    Criação das telas de perfil

--- AppMobile/chronosgram/src/pages/home/perfil/perfil.tsx
+++ AppMobile/chronosgram/src/pages/home/perfil/perfil.tsx
@@ -1,18 +1,70 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, ScrollView, Alert } from 'react-native';
+import axios from 'axios';
 import styles from './style';
 
-export default function Perfil() {
+const Perfil = () => {
+  const [fullName, setFullName] = useState('');
+  const [turnoLivre, setTurnoLivre] = useState('');
+  const userId = '5a0e161c-095c-490f-9a60-f7f1635118fe'; // ID do usuário
+  const baseUrl = 'https://fast-api-x5fr.onrender.com'; // URL do servidor
 
-  const navigation = useNavigation();
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/users/${userId}`);
+        const data = response.data;
+        setFullName(data.nome);
+        setTurnoLivre(data.turno_livre); // Atualiza o turno livre com o valor do banco de dados
+      } catch (error) {
+        console.error('Erro ao buscar dados do usuário: ', error);
+        Alert.alert('Erro', 'Não foi possível buscar os dados do usuário.');
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   return (
-    
-    <View style={styles.container}>
-     
-      <Text> Perfil </Text>
-      
-    </View>
+    <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
+      <View style={styles.profileBox}>
+        <Image source={require('../../../../assets/images/perfil/profile.jpg')} style={styles.profilePicture} />
+        <View style={styles.textContainer}>
+          <Text style={styles.fullName}>{fullName || 'Carregando...'}</Text>
+          <Text style={styles.userInfo}>Idade: 24</Text>
+          <Text style={styles.userInfo}>Gênero: Masculino</Text>
+          <View style={styles.horizontalContainer}>
+            <Text style={styles.username}>@Matth</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.profileBox}>
+        <Image source={require('../../../../assets/images/perfil/logo.jpg')} style={styles.logoPicture} />
+        <View style={styles.textContainer}>
+          <Text style={styles.university}>Universidade Federal Rural de Pernambuco</Text>
+          <View style={styles.horizontalContainer}>
+            <Text style={styles.cityName}>Unidade: Belo jardim</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.profileBox}>
+        <Image source={require('../../../../assets/images/perfil/computação.jpg')} style={styles.coursePicture} />
+        <View style={styles.textContainer}>
+          <Text style={styles.course}>Engenharia da Computação</Text>
+          <View style={styles.horizontalContainer}>
+            <Text style={styles.courseType}>Horário: Integral</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.profileBox}>
+        <Image source={require('../../../../assets/images/perfil/rotina.jpg')} style={styles.typePicture} />
+        <View style={styles.textContainer}>
+          <Text style={styles.userType}>Intensivo</Text>
+          <Text style={styles.turnoLivre}>Turno Livre: {turnoLivre || 'Carregando...'}</Text> 
+        </View>
+      </View>
+    </ScrollView>
   );
-}
+};
+
+export default Perfil;
--- AppMobile/chronosgram/src/pages/home/perfil/style.tsx
+++ AppMobile/chronosgram/src/pages/home/perfil/style.tsx
@@ -1,12 +1,123 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },     
-  });
+  scrollView: {
+    flex: 1,
+    backgroundColor: '#16041B', 
+  },
+  contentContainer: {
+    marginTop: 20, 
+    padding: 20,
+  },
+  profileBox: {
+    backgroundColor: '#73628A', 
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 1,
+    marginTop: 5, 
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  userBox: {
+    backgroundColor: '#73628A', 
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 1,
+    marginTop: 5, 
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  profilePicture: {
+    width: 82,
+    height: 82,
+    marginBottom: 10,
+    borderRadius: 50,
+    marginRight: 20, 
+  },
+  logoPicture: {
+    width: 120,
+    height: 120,
+    borderRadius: 20,
+    marginRight: 20, 
+  },
+  coursePicture: {
+    width: 120,
+    height: 120,
+    borderRadius: 20,
+    marginRight: 20, 
+  },
+  typePicture: {
+    width: 120,
+    height: 120,
+    borderRadius: 20,
+    marginRight: 20, 
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  fullName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#16041B', 
+    marginBottom: 10,
+  },
+  userInfo: {
+    fontSize: 16,
+    color: '#16041B', 
+    marginBottom: 10,
+  },
+  username: {
+    fontSize: 14,
+    color: '#16041B',
+    marginLeft: 140,
+  },
+  userBio: {
+    fontSize: 14,
+    color: '#16041B',
+    marginLeft: 160,
+  },
+  university: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#16041B',
+    marginTop: 5, 
+  },
+  cityName: {
+    fontSize: 16,
+    color: '#16041B',
+    marginTop: 5,
+    flexShrink: 1, 
+  },
+  course: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#16041B', 
+    marginTop: 5, 
+  },
+  courseType: {
+    fontSize: 16,
+    color: '#16041B', 
+    marginTop: 5,
+    flexShrink: 1, 
+  },
+  userType: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#16041B', 
+    marginTop: 5, 
+  },
+  turnoLivre: { 
+    fontSize: 16,
+    color: '#16041B', 
+    marginTop: 5,
+    flexShrink: 1, 
+  },
+});
 
-export default styles
+export default styles;
