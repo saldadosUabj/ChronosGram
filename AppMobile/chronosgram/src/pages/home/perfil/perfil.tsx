7605ace63f9fe153ff6c0ae1d0c11e1ff6bfc638
Matheus Patriota <109482342+Matth-P@users.noreply.github.com>
2024-07-28 20:51:00 -0300

    Update na logica

--- AppMobile/chronosgram/src/pages/home/perfil/perfil.tsx
+++ AppMobile/chronosgram/src/pages/home/perfil/perfil.tsx
@@ -3,19 +3,23 @@ import { View, Text, Image, ScrollView, Alert } from 'react-native';
 import axios from 'axios';
 import styles from './style';
 
+interface UserData {
+  nome: string;
+  turno_livre: string;
+  tipo: string;
+  username: string;
+}
+
 const Perfil = () => {
-  const [fullName, setFullName] = useState('');
-  const [turnoLivre, setTurnoLivre] = useState('');
+  const [userData, setUserData] = useState<UserData | null>(null);
   const userId = '5a0e161c-095c-490f-9a60-f7f1635118fe'; // ID do usuário
   const baseUrl = 'https://fast-api-x5fr.onrender.com'; // URL do servidor
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/users/${userId}`);
-        const data = response.data;
-        setFullName(data.nome);
-        setTurnoLivre(data.turno_livre); // Atualiza o turno livre com o valor do banco de dados
+        const response = await axios.get<UserData>(`${baseUrl}/users/${userId}`);
+        setUserData(response.data);
       } catch (error) {
         console.error('Erro ao buscar dados do usuário: ', error);
         Alert.alert('Erro', 'Não foi possível buscar os dados do usuário.');
@@ -25,16 +29,20 @@ const Perfil = () => {
     fetchUserData();
   }, []);
 
+  if (!userData) {
+    return null; // Retorna nulo até que os dados sejam buscados
+  }
+
   return (
     <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
       <View style={styles.profileBox}>
         <Image source={require('../../../../assets/images/perfil/profile.jpg')} style={styles.profilePicture} />
         <View style={styles.textContainer}>
-          <Text style={styles.fullName}>{fullName || 'Carregando...'}</Text>
+          <Text style={styles.fullName}>{userData.nome}</Text>
           <Text style={styles.userInfo}>Idade: 24</Text>
           <Text style={styles.userInfo}>Gênero: Masculino</Text>
           <View style={styles.horizontalContainer}>
-            <Text style={styles.username}>@Matth</Text>
+            <Text style={styles.username}>{userData.username}</Text>
           </View>
         </View>
       </View>
@@ -59,8 +67,8 @@ const Perfil = () => {
       <View style={styles.profileBox}>
         <Image source={require('../../../../assets/images/perfil/rotina.jpg')} style={styles.typePicture} />
         <View style={styles.textContainer}>
-          <Text style={styles.userType}>Intensivo</Text>
-          <Text style={styles.turnoLivre}>Turno Livre: {turnoLivre || 'Carregando...'}</Text> 
+          <Text style={styles.userType}>Tipo de usuário: {userData.tipo}</Text>
+          <Text style={styles.turnoLivre}>Turno Livre: {userData.turno_livre}</Text>
         </View>
       </View>
     </ScrollView>
