5ce21d4a59a3b30f39644a0418ff10105828b7d8
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2024-10-25 18:04:20 -0300

    splash screen

--- AppMobile/chronosgram/App.tsx
+++ AppMobile/chronosgram/App.tsx
@@ -1,28 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 import Routes from './src/routers/routes';
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     "Nunito-Sans": require("./assets/fonts/NunitoSans.ttf"),
     "Nunito-Sans-Italic": require("./assets/fonts/NunitoSans-Italic.ttf"),
     "Josefins-Sans": require("./assets/fonts/JosefinsSans.ttf"),
     "Josefins-Sans-Italic": require("./assets/fonts/JosefinSans-Italic.ttf"),
     "Josefins-Sans-Bold": require("./assets/fonts/JosefinSans-Bold.ttf"),
-  })
+  });
+
+  // Show a loading indicator while fonts are loading
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
-    <Routes/>
+    <Routes />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
